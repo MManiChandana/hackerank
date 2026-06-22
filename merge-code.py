a9e8c9298f0a1cb986d2f8c711eef3b1d2cde013
MManiChandana <manichandana2612@gmail.com>
2026-06-22 21:23:12 +0530

    commit

---
+++ merge-code.py
@@ -0,0 +1,15 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        substring = string[i:i + k]
+
+        unique = ""
+        for ch in substring:
+            if ch not in unique:
+                unique += ch
+
+        print(unique)
+
+if __name__ == '__main__':
+    string = input()
+    k = int(input())
+    merge_the_tools(string, k)
